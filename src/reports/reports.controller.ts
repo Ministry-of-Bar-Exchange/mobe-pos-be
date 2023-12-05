d4ffa254de81d8387f592c4abc13a1c81987aaeb
Arun Kashyap <arunk@keymouseit.com>
2023-12-05 11:15:50 +0530

    Added Reports Module

---
+++ src/reports/reports.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Query } from "@nestjs/common";
+import { ReportsService } from "./reports.service";
+import { CommonObjectType } from "types";
+
+@Controller("reports")
+export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
+  @Get()
+  getAllItems(@Query() filters: any) {
+    return this.reportsService.getAllItems(filters);
+  }
+
+  @Get('/steward')
+  getAllSaleBySteward(@Query() filters: any) {
+    return this.reportsService.getAllSaleBySteward(filters);
+  }
+
+  @Get('/table')
+  getAllSaleByTable(@Query() filters: any) {
+    return this.reportsService.getAllSaleByTable(filters);
+  }
+}
