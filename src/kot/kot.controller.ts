18e2fb48d5b7b98beb879f84dc62f2faff33b0ce
Arun Kashyap <arunk@keymouseit.com>
2023-12-11 12:35:20 +0530

    Remove Duplicate Item

--- src/kot/kot.controller.ts
+++ src/kot/kot.controller.ts
@@ -24,11 +24,6 @@ export class KotController {
     return this.kotService.getAllKots();
   }
 
-  @Get("all")
-  getAllKots() {
-    return this.kotService.getAllKots();
-  }
-
   @Get(":id")
   readCategory(@Param("id") id: string) {
     return this.kotService.read(id);
